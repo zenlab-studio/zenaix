a7dedb3463a161c68934e0e70dccc086ac67ad84
zenlab-studio <zenlab@zenlab.studio>
2026-07-09 17:23:25 +0200

    Chore: rimosso api/test.js

--- api/test.js
+++
@@ -1,3 +0,0 @@
-module.exports = async (req, res) => {
-  res.status(200).json({ ok: true, env: Object.keys(process.env).filter(k => !k.includes('TOKEN') && !k.includes('SECRET') && !k.includes('KEY')).sort() });
-};
